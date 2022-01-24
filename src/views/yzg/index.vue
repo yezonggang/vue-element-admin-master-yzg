e8aa91ead7100670d6bfe192b043c8492b191b96
yezonggang <717818895@qq.com>
2022-01-24 16:49:49 +0800

    适配magic-api

---
+++ src/views/yzg/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul id="example-1">
+      <li v-for="item in student" :key="item.ID">
+        {{ item.GENDER }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { getstudent } from '@/api/yzg'
+export default {
+  data() {
+    return {
+      student: []
+    }
+  },
+  created() {
+    this.getstudent()
+  },
+  methods: {
+    async getstudent() {
+      const res = await getstudent()
+      this.student = res.data
+    }
+  }
+}
+</script>
+
