3dbca4157b761c76b77f0f62bccbcc4c89de96cf
叶子叶 <717818895@qq.com>
2022-01-26 18:11:40 +0800

    Update index.vue

--- src/views/yzg/index.vue
+++ src/views/yzg/index.vue
@@ -1,28 +1,28 @@
-  <template>
-    <el-table
-      :data="student"
-      border
-      style="width: 100%">
-      <el-table-column
-        prop="NAME"
-        label="姓名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="AGE"
-        label="年级"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="GENDER"
-        label="性别">
-      </el-table-column>
-      <el-table-column
-        prop="CLASS"
-        label="班级信息">
-      </el-table-column>
-    </el-table>
-  </template>
+<template>
+  <el-table
+    :data="student"
+    style="width: 100%"
+  >
+    <el-table-column
+      prop="NAME"
+      label="姓名"
+      width="180"
+    />
+    <el-table-column
+      prop="AGE"
+      label="年级"
+      width="180"
+    />
+    <el-table-column
+      prop="GENDER"
+      label="性别"
+    />
+    <el-table-column
+      prop="CLASS"
+      label="班级信息"
+    />
+  </el-table>
+</template>
 
 <script>
 import { getstudent } from '@/api/yzg'
